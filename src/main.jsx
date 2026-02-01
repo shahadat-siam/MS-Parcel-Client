25cd47c2d9067508a32775f0195ab311801c8bcc
shahadat-siam <lonelinessismycompanion09@gmail.com>
2026-02-01 08:49:29 +0600

    install react  aos

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,11 @@ import App from "./App.jsx";
 import { RouterProvider } from "react-router/dom";
 import { router } from "./router/router.jsx";
 
+import 'aos/dist/aos.css';
+import Aos from "aos";
+
+Aos.init()
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <div className="font-urbanist max-w-7xl mx-auto">
